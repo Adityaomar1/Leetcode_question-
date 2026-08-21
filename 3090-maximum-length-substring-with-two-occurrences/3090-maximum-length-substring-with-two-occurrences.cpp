fc198c3a744b764e7ba4204bdf68ea13a87bbdbc
Aditya_Omar <2k24.csaiml1a2411963@gmail.com>
2026-08-21 11:52:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (52.41%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-        unordered_map<int,int> freq;
+        unordered_map<int,int> mp;
         int low = 0;
         int ans = 0;
         for(int high = 0;high<s.size();high++){
-            freq[s[high]]++;
-            while(freq[s[high]]>2){
-                freq[s[low]]--;
+            mp[s[high]]++;
+            while(mp[s[high]]>2){
+                mp[s[low]]--;
                 low++;
             }
             int len = high-low+1;
